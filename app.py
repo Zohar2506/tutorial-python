43ce8be1ee52d1b0624c87ceff7b3a3ec38ac05d
Zohar <zohar@rookout.com>
2019-03-05 18:19:20 +0200

    fix delete bug

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def index():
 @app.route('/todos/<todoId>', methods=['DELETE'])
 def del_todo(todoId):
     todos = Store.getInstance().todos
-    newTodos = [t for t in todos if t['id'] == todoId]
+    newTodos = [t for t in todos if t['id'] != todoId]
     Store.getInstance().todos = newTodos
     return ('', 204)
 
